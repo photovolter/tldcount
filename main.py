3c83d0bf0a499083a7e289a39949f9dbe49494ef
AMK <a>
2022-03-04 10:29:06 +0100

    Longest/shortest domains list

--- main.py
+++ main.py
@@ -2,23 +2,17 @@ from domainnames import tldlist
 import requests
 import re
 
-# cleantest = re.compile('<.*?>|- ')
-# Zmienna będzie wykorzystana do wyczyszczenia tagów HTML i usunięcia wiodących znaków - (minus).
-# Zapis ('<.*?>|- ') oznacza: weź wzorzec regex "<.*?>" lub "- ".
-# re.compile oznacza kompilację wzorca do obiektu regex, który będzie później wykorzystany w innych funkcjach.
-
-orangutan = 'https://zajecia-programowania-xd.pl/flagi'
-surowe_info = requests.get(orangutan)
-text = surowe_info.text
-cleantest = re.compile('<.*?>|- |:(.*)+')
-efekt = text.rsplit('</p>')
+flagsurl = 'https://zajecia-programowania-xd.pl/flagi'
+rawdata = requests.get(flagsurl)
+text = rawdata.text
+cleantext = re.compile('<.*?>|- |:(.*)+|/(.*)+')
+urlslist = text.rsplit('</p>')
 letterslist = 'abcdefghijklmnopqrstuvwxyz.'
 
-def listtostring(listjoin):
-    return (''.join(listjoin))
 
+def by_size(words, size):
+    return [word for word in words if len(word) == size]
 
-# Funkcja maxstr wykorzystuje wbudowane funkcje min i max do znalezienia najdłuższego i najkrótszego elementu na liście.
 
 def maxstr(tocheck):
     maxlen = max(tocheck, key=len)
@@ -26,28 +20,12 @@ def maxstr(tocheck):
     return [minlen, maxlen]
 
 
-# Funkcja splittld.
-# FQDN - pełna nazwa domenowa. Np www.onet.pl lub www.moja.domena.com.pl
-# splitted - lista elementów FQDN powstała po podziale w miejscu kropki - ['www', 'onet', 'pl'], ['www', 'moja', 'domena', 'com', 'pl']
-# myjoin - sklejony ostatni i przedostatni człon FQDN - onet.pl, com.pl
-# tld - ostatni człon FQDN - pl
-# Funkcja zwraca listę 2 wartości, np: ['onet', 'pl'] lub ['com', 'pl']
-
 def splittld(tosplit):
     splitted = tosplit.split('.')
     myjoin = splitted[-2] + '.' + splitted[-1]
     tld = splitted[-1]
     return [myjoin, tld]
 
-
-# Funkcja validateDomain. Argumentem jest domniemana nazwa domeny
-# Funkcja zwróci wartoś false jeżeli ciąg znaków jest pusty lub nie pasuje do wzorca.
-# W pozostałych przypadkach zwróci wartość true.
-#
-# 1. Wyrażenie regularne sprawdzające czy ciąg znaków jest domeną.
-# 2. Kompilacja wyrażenia regularnego w celu jego późniejszego użycia.
-# 3. Sprawdzamy czy string nie jest pusty, zwracamy false, jeżeli jest.
-# 4. Użycie mudułu Pythona re (regex) w celu dopasowania ciągu znaków do wzorca.
 
 def validatedomain(domainname):
     regex = "^((?!-)[A-Za-z0-9-]" + "{1,63}(?<!-)\\.)" + "+[A-Za-z]{2,6}"
@@ -61,38 +39,34 @@ def validatedomain(domainname):
 
 
 newefekt = []
+for i, linia in enumerate(urlslist):
+    urlslist[i] = re.sub(cleantext, '', linia)
+    if validatedomain(urlslist[i]):
+        newefekt.append(urlslist[i])
 
 
-# Teraz już z górki
-# Iterujemy sobie po naszej liści, która powstała w wyniku text.rsplit('</p>')
-# 1. Każdą linię czyścimy z tagów HTML i śmieci w postaci myślników.
-# 2. Sprawdzamy, czy ta już oczyszczona linia pasuje do wzorca domeny.
-# 4. Jeżeli pasuje do wzorca, to wrzucamy ją na nową listę. W ten sposób powstaje wyłącznie lista domen.
-
-for i, linia in enumerate(efekt):
-    efekt[i] = re.sub(cleantest, '', linia)
-    if validatedomain(efekt[i]):
-        newefekt.append(efekt[i])
-
-
-
-stringfromlist = listtostring(newefekt).lower()
 lettersdict = {}
 for letter in letterslist:
-    lettersdict[letter] = stringfromlist.count(letter)
+    lettersdict[letter] = str(newefekt).lower().count(letter)
 
-
-# W następnych 2 liniach wykorzystano mechanizm List Comprehension.
-# Jest to inna metoda zapisu pętli for.
 sumapl = sum(1 for i, s in enumerate(newefekt) if splittld(s)[0] not in tldlist and splittld(s)[1]=='pl')
 sumapltld = sum(1 for i, s in enumerate(newefekt) if splittld(s)[0] in tldlist)
+sumanotpl = sum(1 for i, s in enumerate(newefekt) if splittld(s)[0] not in tldlist and splittld(s)[1]!='pl')
+
+shortestdomainsize = len(maxstr(newefekt)[0])
+longestdomainsize = len(maxstr(newefekt)[1])
+shortestdomain = by_size(newefekt, shortestdomainsize)
+longestdomain = by_size(newefekt, longestdomainsize)
 
 print()
 print(f"Flagi w domenie pl, bez domeny drugiego poziomu: {sumapl}")
 print(f"Flagi w domenie pl z domeną drugiego poziomu: {sumapltld}")
+print(f"Flagi poza domeną pl: {sumanotpl}")
 print()
-print(f"Najkrótsza nazwa domeny: {len(maxstr(newefekt)[0])}")
-print(f"Najkrótsza nazwa domeny: {maxstr(newefekt)[1]}")
+print(f"Najkrótsza nazwa domeny: {shortestdomainsize} znaków")
+print(*shortestdomain)
+print(f"Najdłuższa nazwa domeny: {longestdomainsize} znaków")
+print(*longestdomain)
 print()
 print('Liczebność znaków alfabetu:')
 
